755d6cc659e24c2d3d0d4937c15cd651b124a29e
Maciej <maciej.beresewicz@gmail.com>
2023-10-28 12:07:40 +0200

    update and tests

--- inst/tinytest/test_blocking.R
+++ inst/tinytest/test_blocking.R
@@ -17,7 +17,11 @@ df_example <- data.frame(txt = c(
 # check arguments ---------------------------------------------------------
 
 expect_silent(
-  blocking(x = df_example$txt, verbose = F, ann = "hnsw")
+  blocking(x = df_example$txt,
+           verbose = F,
+           ann = "hnsw",
+           control_ann = controls_ann(hnsw=list(M = 5, ef_s = 10, ef_b = 10),
+                                      distance = "cosine"))
 )
 
 
@@ -25,12 +29,17 @@ expect_silent(
 
 
 expect_equal(
-  as.numeric(blocking(x = df_example$txt, ann = "hnsw")),
+  as.numeric(blocking(x = df_example$txt,
+                      verbose = F,
+                      ann = "hnsw",
+                      control_ann = controls_ann(hnsw = list(M = 5, ef_s = 10, ef_b = 10),
+                                                 distance = "cosine"))),
   c(1, 1, 1, 1, 2, 2, 2, 2)
 )
 
 expect_equal(
-  as.numeric(blocking(x = df_example$txt, ann = "lsh")),
+  as.numeric(blocking(x = df_example$txt, ann = "lsh"),
+             control_ann = controls_ann(distance = "cosine")),
   c(1, 1, 1, 1, 2, 2, 2, 2)
 )
 
